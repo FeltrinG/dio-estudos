2ee88aa948e9d040b89849bae19ec64407219ce3
FeltrinG <Gabriel.feltrin_@outlook.com>
2024-05-31 14:51:29 -0300

    Projeto-finalizado.

--- Calculadora/src/AboutMe.java
+++ Calculadora/src/AboutMe.java
@@ -1,7 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-    
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //criando o objeto scanner
+        Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome");
         String nome = scanner.next();
